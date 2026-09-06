ae1e850acc983400c3a9af93681ca124e29db553
Marko Samardzic <samardzicm33@gmail.com>
2026-09-06 16:19:36 +0200

    Modul 0: inicijalni setup - Spring Boot, PostgreSQL, Flyway

---
+++ backend/src/test/java/ba/sum/kviz/KvizApplicationTests.java
@@ -0,0 +1,13 @@
+package ba.sum.kviz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KvizApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
